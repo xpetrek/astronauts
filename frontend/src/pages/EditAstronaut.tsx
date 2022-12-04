a3249e590af227782f0f93b5fe80d730a44c66eb
Stanislav Petrek <stanislav.petrek@tietoevry.com>
2022-12-04 20:35:06 +0100

    Introduced create/edit astronaut page

--- frontend/src/pages/EditAstronaut.tsx
+++ frontend/src/pages/EditAstronaut.tsx
@@ -1,3 +1,181 @@
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Grid,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ColorResult, SketchPicker } from "react-color";
+
+import { AstronautColorSvg } from "../components/AstronautColorSvg";
+import { AstronautDraft, ColorPalette } from "../utils/types";
+import { useAstronauts } from "../hooks/useAstronauts";
+
 export const EditAstronaut = () => {
-  return <></>;
+  const [astronaut, setAstronaut] = useState<AstronautDraft>({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    superpower: "",
+  });
+  const [color, setColor] = useState("");
+  const [isHelmDown, setIsHelmDown] = useState(false);
+  const [currentPart, setCurrentPart] = useState<keyof ColorPalette>("helmet");
+  const [colorPalette, setColorPalette] = useState({
+    helmet: "#fff",
+    backpack: "#fff",
+    suit: "#fff",
+    gloves: "#fff",
+    pants: "#fff",
+    boots: "#fff",
+  });
+
+  useEffect(() => {
+    setColor(colorPalette[currentPart]);
+  }, [currentPart, colorPalette]);
+
+  const handleChangeColor = (pickedColor: ColorResult) => {
+    setColor(pickedColor.hex);
+    setColorPalette((old) => ({ ...old, [currentPart]: pickedColor.hex }));
+  };
+
+  const handleChangePart = (clickedOn: keyof ColorPalette) => {
+    setCurrentPart(clickedOn);
+  };
+
+  const handleHelmetPosition = () => {
+    setIsHelmDown((old) => !old);
+  };
+
+  const handleCreateAstronaut = () => {
+    useAstronauts.newAstronaut(astronaut);
+  };
+  
+  const handleChangeFilterChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {};
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAstronaut((old) => ({ ...old, [e.target.name]: e.target.value }));
+    console.log(astronaut);
+  };
+
+  return (
+    <>
+      <Typography variant="h1">Add new astronaut</Typography>
+      <Grid container direction="row" spacing={2}>
+        <Grid item xs={12} sm={12} md={7} lg={7} xl={7}>
+          <Box
+            sx={{
+              width: "70%",
+              marginLeft: 10,
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <AstronautColorSvg
+              colorPalette={colorPalette}
+              isHelmDown={isHelmDown}
+              handleClick={handleChangePart}
+            />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={5}
+          lg={5}
+          xl={5}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingRight: 5,
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              paddingBottom: "10px",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
+              }}
+            >
+              {Object.keys(colorPalette).map((part) => (
+                <Button
+                  variant={currentPart === part ? "contained" : "outlined"}
+                  onClick={() => handleChangePart(part as keyof ColorPalette)}
+                >
+                  {part}
+                </Button>
+              ))}
+            </Box>
+            <SketchPicker
+              width="50%"
+              color={color}
+              onChange={handleChangeColor}
+            />
+          </Box>
+          <Box>
+            <TextField
+              name="firstName"
+              sx={{ width: "100%", paddingTop: "5px" }}
+              label={"First Name"}
+              onChange={handleInputChange}
+            />
+            <TextField
+              name="lastName"
+              sx={{ width: "100%", paddingTop: "5px" }}
+              label={"Last Name"}
+              onChange={handleInputChange}
+            />
+            <TextField
+              name="dateOfBirth"
+              sx={{ width: "100%", paddingTop: "5px" }}
+              label={"Date of birth"}
+              onChange={handleInputChange}
+            />
+            <TextField
+              name="superpower"
+              sx={{ width: "100%", paddingTop: "5px" }}
+              label={"Superpower"}
+              onChange={handleInputChange}
+            />
+          </Box>
+          <Box
+            sx={{
+              paddingTop: "10px",
+              width: "80%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <FormControlLabel
+              control={
+                <Switch checked={isHelmDown} onChange={handleHelmetPosition} />
+              }
+              label="Helmet"
+            />
+            <Button variant="contained" onClick={handleCreateAstronaut}>
+              Create astronaut
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
